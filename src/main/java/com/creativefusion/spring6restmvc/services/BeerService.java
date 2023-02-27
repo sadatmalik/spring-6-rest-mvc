7bf696db731f3ad92a673d5bc6493151ab320e3c
sadat.malik <sadat.malik@me.com>
2023-02-27 22:35:15 +0000

    adding get all endpoint

--- src/main/java/com/creativefusion/spring6restmvc/services/BeerService.java
+++ src/main/java/com/creativefusion/spring6restmvc/services/BeerService.java
@@ -2,11 +2,13 @@ package com.creativefusion.spring6restmvc.services;
 
 import com.creativefusion.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @author sm@creativefusion.net
  */
 public interface BeerService {
+    List<Beer> listBeers();
     Beer getBeerById(UUID id);
 }
