abd4a4251937a7bc5a9ec560b2a478e9cde3ce08
sadat.malik <sadat.malik@me.com>
2023-02-27 21:55:20 +0000

    adding lombok pojo

---
+++ src/main/java/com/creativefusion/spring6restmvc/model/Beer.java
@@ -0,0 +1,23 @@
+package com.creativefusion.spring6restmvc.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * @author sm@creativefusion.net
+ */
+@Data
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
