efaac0997b2b03c33a1befce4de7bdf514b6044f
sadat.malik <sadat.malik@me.com>
2023-03-11 21:25:05 +0000

    throw an exception using mockito

--- src/test/java/com/creativefusion/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/creativefusion/spring6restmvc/controllers/BeerControllerTest.java
@@ -90,6 +90,14 @@ class BeerControllerTest {
     }
 
     @Test
+    void getBeerByIdNotFound() throws Exception {
+        given(beerService.getBeerById(any(UUID.class))).willThrow(NotFoundException.class);
+
+        mockMvc.perform(get(BeerController.BEER_PATH_ID, UUID.randomUUID()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     void testListBeers() throws Exception {
         given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
 
