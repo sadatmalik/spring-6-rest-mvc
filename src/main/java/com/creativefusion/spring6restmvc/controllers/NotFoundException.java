677127a8ecbd87647f5e27360712e281d4b7c635
sadat.malik <sadat.malik@me.com>
2023-03-11 21:46:13 +0000

    use response status annotation instead of controller advice for exception responses

--- src/main/java/com/creativefusion/spring6restmvc/controllers/NotFoundException.java
+++ src/main/java/com/creativefusion/spring6restmvc/controllers/NotFoundException.java
@@ -1,8 +1,12 @@
 package com.creativefusion.spring6restmvc.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author sm@creativefusion.net
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
