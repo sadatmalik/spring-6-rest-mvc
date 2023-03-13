54c5e38afe4dda4c4808994d007b24ed2109273a
sadat.malik <sadat.malik@me.com>
2023-03-13 17:48:21 +0000

    adding controller integration test

---
+++ src/test/java/com/creativefusion/spring6restmvc/controllers/BeerControllerIT.java
@@ -0,0 +1,41 @@
+package com.creativefusion.spring6restmvc.controllers;
+
+import com.creativefusion.spring6restmvc.model.BeerDTO;
+import com.creativefusion.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+
+}
