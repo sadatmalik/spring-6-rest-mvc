3f6ff9ad7e732cbb2537790d1a37e26d6ddcea25
sadat.malik <sadat.malik@me.com>
2023-03-16 22:14:43 +0000

    adding controller dto validation bindings and validation unit test

--- src/main/java/com/creativefusion/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/creativefusion/spring6restmvc/controllers/BeerController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,7 +26,7 @@ public class BeerController {
     private final BeerService beerService;
 
     @PostMapping(BEER_PATH)
-    public ResponseEntity<Void> handlePost(@RequestBody BeerDTO beer){
+    public ResponseEntity<Void> handlePost(@Validated @RequestBody BeerDTO beer){
         BeerDTO savedBeer = beerService.saveNewBeer(beer);
 
         HttpHeaders headers = new HttpHeaders();
