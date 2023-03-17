e2fb32b841e2b7fb1ba157b2640bbff998d576df
sadat.malik <sadat.malik@me.com>
2023-03-17 09:14:46 +0000

    customise error response in controller advice for validation exceptions

--- src/main/java/com/creativefusion/spring6restmvc/controllers/CustomErrorController.java
+++ src/main/java/com/creativefusion/spring6restmvc/controllers/CustomErrorController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author sm@creativefusion.net
@@ -15,9 +18,15 @@ import java.util.List;
 public class CustomErrorController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<List<FieldError>> handleBindErrors(MethodArgumentNotValidException exception){
-        return ResponseEntity.badRequest()
-                             .body(exception.getBindingResult()
-                                            .getFieldErrors());
+    ResponseEntity<List<Map<String, String>>> handleBindErrors(MethodArgumentNotValidException exception) {
+
+        List<Map<String, String>> errorList = exception.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String > errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errorList);
     }
 }
