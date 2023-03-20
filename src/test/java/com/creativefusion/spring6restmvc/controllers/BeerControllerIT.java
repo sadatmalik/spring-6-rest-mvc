034719dd1c6f152b2fd83dff0024aae933a521ae
sadat.malik <sadat.malik@me.com>
2023-03-20 21:13:55 +0000

    adding integration test to list using query parameter

--- src/test/java/com/creativefusion/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/com/creativefusion/spring6restmvc/controllers/BeerControllerIT.java
@@ -29,6 +29,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -117,6 +118,14 @@ class BeerControllerIT {
         beerRepository.deleteAll();
         List<BeerDTO> dtos = beerController.listBeers();
         assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void tesListBeersByName() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH)
+                        .queryParam("beerName", "IPA"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(100)));
     }
 
     @Test
