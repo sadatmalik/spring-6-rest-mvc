0d65c17283654f3e64117c7ff769fd4e48ba8c1f
sadat.malik <sadat.malik@me.com>
2023-03-25 20:34:35 +0000

    adding beer order repository and unit test

---
+++ src/test/java/com/creativefusion/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.creativefusion.spring6restmvc.repositories;
+
+import com.creativefusion.spring6restmvc.entities.Beer;
+import com.creativefusion.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().get(0);
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Test
+    void testBeerOrders() {
+        System.out.println(beerOrderRepository.count());
+        System.out.println(customerRepository.count());
+        System.out.println(beerRepository.count());
+        System.out.println(testCustomer.getName());
+        System.out.println(testBeer.getBeerName());
+    }
+}
