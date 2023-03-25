09c4e37fa1c16b5083c96500e34499eda89eafb6
sadat.malik <sadat.malik@me.com>
2023-03-25 20:08:20 +0000

    adding customer-order one-to-many mapping

--- src/main/java/com/creativefusion/spring6restmvc/entities/Customer.java
+++ src/main/java/com/creativefusion/spring6restmvc/entities/Customer.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +16,7 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -43,4 +45,7 @@ public class Customer {
     private Integer version;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
 }
