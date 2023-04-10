85c4f57b89a1f249feda51888ae67f1621f464fe
sadat.malik <sadat.malik@me.com>
2023-04-10 14:54:40 +0300

    disable csrf on api endpoints

---
+++ src/main/java/com/creativefusion/spring6restmvc/config/SpringSecConfig.java
@@ -0,0 +1,24 @@
+package com.creativefusion.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author sm@creativefusion.net
+ */
+@Configuration
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic(Customizer.withDefaults())
+                .csrf().ignoringRequestMatchers("/api/**");
+        return http.build();
+    }
+
+}
