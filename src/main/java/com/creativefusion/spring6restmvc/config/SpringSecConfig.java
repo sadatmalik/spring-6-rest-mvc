914ee754e42fe5dfbe71831c860353ebcde41028
sadat.malik <sadat.malik@me.com>
2023-04-12 22:31:11 +0100

    adding oauth2 to security configuration

--- src/main/java/com/creativefusion/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/creativefusion/spring6restmvc/config/SpringSecConfig.java
@@ -16,8 +16,7 @@ public class SpringSecConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .anyRequest().authenticated()
-                .and().httpBasic(Customizer.withDefaults())
-                .csrf().ignoringRequestMatchers("/api/**");
+                .and().oauth2ResourceServer().jwt();
         return http.build();
     }
 
