e222fb6288fd47c050bbbaa1158a7c2afbbfaa23
sadatmalik <sadat.malik@me.com>
2023-04-29 17:36:05 +0100

    setting alternative security config for rest assured test

--- src/main/java/com/creativefusion/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/creativefusion/spring6restmvc/config/SpringSecConfig.java
@@ -2,13 +2,14 @@ package com.creativefusion.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author sm@creativefusion.net
  */
+@Profile("!test")
 @Configuration
 public class SpringSecConfig {
 
